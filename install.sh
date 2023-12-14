48d030fbd059c08e0a8fb314b291e0b5d0668d95
Jens Kulka <kulkajens82@gmail.com>
2023-12-14 11:55:50 +0100

    Updated install.sh

--- install.sh
+++ install.sh
@@ -41,12 +41,12 @@ insert_alias() {
 	echo "alias $ali_valgrind=\"bash $path/start.sh\"" >> "$sh"
 }
 
-echo -en "Please enter an alias to init your dockercontainer [di]:"
+echo -en "Please enter an alias to init your dockercontainer [d_init]:"
 read -r ali_init
 create_alias "$ali_init" "d_init"
 ali_init="$ali"
 
-echo -en "Please enter an alias to start your dockercontainer [dv]:"
+echo -en "Please enter an alias to start your dockercontainer [d_start]:"
 read -r ali_valgrind
 create_alias "$ali_valgrind" "d_start"
 ali_valgrind="$ali"
